90d6bc95c45f37b22820a07c7c5d5e39e278bbe4
Clem0908 <clementlyonnet@hotmail.fr>
2021-04-10 17:38:14 +0200

    first push

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.Window;
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.Rectangle;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		Frame mainFrame = new Frame(null);
+		Rectangle mainScreen = new Rectangle();
+		f.setLocation(mainScreen.x, mainScreen.y);
+	}
+}
